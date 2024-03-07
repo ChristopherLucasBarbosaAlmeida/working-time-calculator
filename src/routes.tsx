1ae0d3ab5536c1eb65eec0f6626112569e84cae0
ChristopherLucasBarbosaAlmeida <lucaschristofher05@gmail.com>
2024-03-07 13:27:46 -0300

    feat: add page to list all jobs

--- src/routes.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter } from "react-router-dom";
-import { Home } from "./pages";
-
-export const router = createBrowserRouter([
-  {
-    index: true,
-    element: <Home />,
-  },
-]);
